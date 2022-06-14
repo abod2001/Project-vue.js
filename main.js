32a79ec5233e4372d4ccd9507d9fff9d2c4f8806
Abdul Rahman Jihad Muheisen <75423647+abod2001@users.noreply.github.com>
2022-06-14 20:36:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const myVue = new Vue({
+      el: "#abood",
+      data: {
+        width: 300,
+        imageBlur: 0,
+        imageOpacity: 100,
+        imageBorderWidth: 3,
+        imageBorderRadius: 5,
+        imageBorderColor: "#289f47",
+      },
+    });
